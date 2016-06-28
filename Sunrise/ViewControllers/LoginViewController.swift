3a37b0343eeace94ee6dee7308787d2d2fc5ec1d
Nikola Mladenovic <nikola@mladenovic.biz>
2016-06-28 12:39:54 +0200

    Closes #35 Set email keyboard type for login

--- Sunrise/ViewControllers/LoginViewController.swift
+++ Sunrise/ViewControllers/LoginViewController.swift
@@ -27,7 +27,8 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        emailField.keyboardType = .EmailAddress
         loginFormView.layer.borderColor = borderColor.CGColor
         viewModel = LoginViewModel()
     }
