129c56666a5ea89a82750e17926c6a984e1f9ef1
Nikola Mladenovic <nikola@mladenovic.biz>
2016-08-23 11:10:21 +0200

    Closes #54 Updating line item quantities on cart tab

--- Sunrise/Cells/CartLineItemCell.swift
+++ Sunrise/Cells/CartLineItemCell.swift
@@ -3,16 +3,22 @@
 //
 
 import UIKit
+import IQDropDownTextField
 
 class CartLineItemCell: UITableViewCell {
 
     @IBOutlet weak var productNameLabel: UILabel!
     @IBOutlet weak var productImageView: UIImageView!
-    @IBOutlet weak var oldPriceLabel: UILabel!    
+    @IBOutlet weak var oldPriceLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
-    @IBOutlet weak var quantityLabel: UILabel!
+    @IBOutlet weak var quantityLabel: UILabel?
+    @IBOutlet weak var quantityField: IQDropDownTextField?
     @IBOutlet weak var totalPriceLabel: UILabel!
     @IBOutlet weak var skuLabel: UILabel!
     @IBOutlet weak var sizeLabel: UILabel!
+
+    @IBAction func editQuantity(sender: AnyObject) {
+        quantityField?.becomeFirstResponder()
+    }    
 
 }
