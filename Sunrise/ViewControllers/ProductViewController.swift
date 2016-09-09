66f37f75f61066df2bc773e204d807d6a558ca4a
Nikola Mladenovic <nikola@mladenovic.biz>
2016-09-09 15:48:12 +0200

    Related #96 Added support for refreshing PDP

--- Sunrise/ViewControllers/ProductViewController.swift
+++ Sunrise/ViewControllers/ProductViewController.swift
@@ -110,6 +110,7 @@ class ProductViewController: UITableViewController {
                     SVProgressHUD.show()
                 } else {
                     self?.tableView.reloadData()
+                    self?.refreshControl?.endRefreshing()
                     SVProgressHUD.dismiss()
                 }
             })
@@ -229,6 +230,12 @@ class ProductViewController: UITableViewController {
             case 2: return 55
             default: return 0
         }
+    }
+
+    // MARK: - Refreshing
+    
+    @IBAction func refresh(sender: UIRefreshControl) {
+        viewModel?.refreshObserver.sendNext()
     }
 
     // MARK: - Navigation
