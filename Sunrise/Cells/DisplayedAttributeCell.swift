f7dbd16d6790e891c13deb9c1ba7f6441856be93
Nikola Mladenovic <nikola@mladenovic.biz>
2016-09-09 03:16:59 +0200

    Closes #96 Added support for configurable displayable attributes

--- Sunrise/Cells/DisplayedAttributeCell.swift
+++ Sunrise/Cells/DisplayedAttributeCell.swift
@@ -6,15 +6,7 @@ import UIKit
 
 class DisplayedAttributeCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    @IBOutlet weak var attributeValue: UILabel!
+    @IBOutlet weak var attributeKey: UILabel!
 
 }
