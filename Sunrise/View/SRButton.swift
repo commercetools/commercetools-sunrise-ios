594a11094da0ccbf8d7745a4bbe08e4f963dc41e
Nikola Mladenovic <nikola@mladenovic.biz>
2017-09-21 19:50:54 +0200

    Updated color scheme for block 1

---
+++ Sunrise/View/SRButton.swift
@@ -0,0 +1,13 @@
+//
+//  SRButton.swift
+//  Sunrise
+//
+//  Created by Nikola Mladenovic on 9/20/17.
+//  Copyright © 2017 Commercetools. All rights reserved.
+//
+
+import UIKit
+
+class SRButton: UIButton {
+
+}
