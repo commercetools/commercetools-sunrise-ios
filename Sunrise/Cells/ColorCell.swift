0892041368e0729199da5bae8fdb007c90f8571c
Nikola Mladenovic <nikola@mladenovic.biz>
2017-10-24 19:23:51 +0200

    Active filter components

---
+++ Sunrise/Cells/ColorCell.swift
@@ -0,0 +1,10 @@
+//
+// Copyright (c) 2017 Commercetools. All rights reserved.
+//
+
+import UIKit
+
+class ColorCell: UICollectionViewCell {
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var selectedColorImageView: UIImageView!
+}
--- Sunrise/ViewControllers/Products/FiltersViewController.swift
+++ Sunrise/ViewControllers/Products/FiltersViewController.swift
@@ -31,15 +31,19 @@ extension FiltersViewController: UICollectionViewDataSource {
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if collectionView == brandsCollectionView {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BrandCell", for: indexPath)
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BrandCell", for: indexPath) as! BrandCell
+            cell.selectedBrandImageView.isHidden = indexPath.item != 0
             return cell
             
         } else if collectionView == sizesCollectionView {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SizeCell", for: indexPath)
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SizeCell", for: indexPath) as! SizeCell
+            cell.selectedSizeImageView.isHidden = indexPath.item != 0
+            cell.sizeLabel.textColor = indexPath.item == 0 ? .white : UIColor(red:0.16, green:0.20, blue:0.25, alpha:1.0)
             return cell
             
         } else {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ColorCell", for: indexPath)
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ColorCell", for: indexPath) as! ColorCell
+            cell.selectedColorImageView.isHidden = indexPath.item != 0
             return cell
         }
     }
