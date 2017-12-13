34ff620d3815bc2d430a2380be70272268299811
Nikola Mladenovic <nikola@mladenovic.biz>
2017-12-13 15:02:40 +0100

    Block 4 design

--- Sunrise/ViewControllers/SunriseTabBarController.swift
+++ Sunrise/ViewControllers/SunriseTabBarController.swift
@@ -159,14 +159,7 @@ class SunriseTabBarController: UITabBarController {
 extension SunriseTabBarController: UITabBarControllerDelegate {
 
     func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
-        // Until placing custom search view controller, invoke search bar on home tab.
-        if viewController == tabBarController.viewControllers?[AppRouting.TabIndex.searchTab.index] {
-            AppRouting.switchToSearch(becomeFirstResponder: true)
-            return false
-
-        } else {
-            return true
-        }
+        return true
     }
 }
 
