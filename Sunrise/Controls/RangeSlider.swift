cc0f24e4db81744d4dc952801885d1924e13c029
Nikola Mladenovic <nikola@mladenovic.biz>
2018-01-26 13:27:12 -0600

    Filters integration

--- Sunrise/Controls/RangeSlider.swift
+++ Sunrise/Controls/RangeSlider.swift
@@ -230,5 +230,7 @@ public class RangeSlider: UIControl {
     override public func endTracking(_ touch: UITouch?, with event: UIEvent?) {
         lowerThumbLayer.highlighted = false
         upperThumbLayer.highlighted = false
+
+        sendActions(for: .editingDidEnd)
     }
 }
