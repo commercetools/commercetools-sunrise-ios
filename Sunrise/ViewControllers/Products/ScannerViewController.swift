9ce23bec0067b861a131cd20fdb4fcb8d9e0a871
Nikola Mladenovic <nikola@mladenovic.biz>
2018-03-06 11:18:55 +0100

    Barcode scanner

--- Sunrise/ViewControllers/Products/ScannerViewController.swift
+++ Sunrise/ViewControllers/Products/ScannerViewController.swift
@@ -43,8 +43,8 @@ class ScannerViewController: UIViewController {
         }
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
 
         if captureSession == nil {
             captureSession = AVCaptureSession()
