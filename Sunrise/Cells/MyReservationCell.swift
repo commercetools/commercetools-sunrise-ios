8218857be4693e9242f04c61473844c68ba706d4
Nikola Mladenovic <nikola@mladenovic.biz>
2018-04-02 12:25:14 +0200

    My Reservation Details

---
+++ Sunrise/Cells/MyReservationCell.swift
@@ -0,0 +1,12 @@
+//
+// Copyright (c) 2018 Commercetools. All rights reserved.
+//
+
+import UIKit
+
+class MyReservationCell: UITableViewCell {
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var reservationDate: UILabel!
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+}
