2325efce02166757bad48232c08ffaf18e9722a2
Nikola Mladenovic <nikola@mladenovic.biz>
2018-10-23 18:04:36 +0200

    Add Siri shortcut button

--- Sunrise/ViewControllers/Profile/OrderDetailsViewController.swift
+++ Sunrise/ViewControllers/Profile/OrderDetailsViewController.swift
@@ -5,6 +5,7 @@
 import UIKit
 import ReactiveCocoa
 import ReactiveSwift
+import IntentsUI
 
 class OrderDetailsViewController: UIViewController {
 
@@ -13,7 +14,9 @@ class OrderDetailsViewController: UIViewController {
     @IBOutlet weak var expectedDeliveryLabel: UILabel!
     @IBOutlet weak var deliveryAddressLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var cardInfoLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var orderSummaryView: UIView!
     
     @IBOutlet weak var closeButtonTopSpaceConstraint: NSLayoutConstraint!
     @IBOutlet weak var tableViewHeightConstraint: NSLayoutConstraint!
@@ -67,6 +70,22 @@ class OrderDetailsViewController: UIViewController {
             self.scrollViewContentHeightConstraint.constant += newHeight - self.tableViewHeightConstraint.constant
             self.tableViewHeightConstraint.constant = newHeight
         }
+
+        if #available(iOS 12.0, *), let shortcut = INShortcut(intent: viewModel.orderIntent) {
+            addSiriShortcutButton(for: shortcut)
+        }
+    }
+
+    @available(iOS 12.0, *)
+    private func addSiriShortcutButton(for shortcut: INShortcut) {
+        let addShortcutButton = INUIAddVoiceShortcutButton(style: .whiteOutline)
+        addShortcutButton.shortcut = shortcut
+        addShortcutButton.delegate = self
+
+        addShortcutButton.translatesAutoresizingMaskIntoConstraints = false
+        orderSummaryView.addSubview(addShortcutButton)
+        addShortcutButton.topAnchor.constraint(equalTo: cardInfoLabel.bottomAnchor, constant: 30).isActive = true
+        orderSummaryView.centerXAnchor.constraint(equalTo: addShortcutButton.centerXAnchor).isActive = true
     }
 
     @IBAction func closeOrderDetails(_ sender: UIButton) {
@@ -86,6 +105,51 @@ extension OrderDetailsViewController: UITableViewDataSource {
         cell.quantityLabel.text = viewModel.lineItemQuantity(at: indexPath)
         cell.priceLabel.text = viewModel.lineItemPrice(at: indexPath)
         return cell
+    }
+}
+
+@available(iOS 12.0, *)
+extension OrderDetailsViewController: INUIAddVoiceShortcutButtonDelegate {
+    func present(_ addVoiceShortcutViewController: INUIAddVoiceShortcutViewController, for addVoiceShortcutButton: INUIAddVoiceShortcutButton) {
+        addVoiceShortcutViewController.delegate = self
+        present(addVoiceShortcutViewController, animated: true)
+    }
+
+    func present(_ editVoiceShortcutViewController: INUIEditVoiceShortcutViewController, for addVoiceShortcutButton: INUIAddVoiceShortcutButton) {
+        editVoiceShortcutViewController.delegate = self
+        present(editVoiceShortcutViewController, animated: true)
+    }
+}
+
+@available(iOS 12.0, *)
+extension OrderDetailsViewController: INUIAddVoiceShortcutViewControllerDelegate {
+    func addVoiceShortcutViewController(_ controller: INUIAddVoiceShortcutViewController, didFinishWith voiceShortcut: INVoiceShortcut?, error: Error?) {
+        if let error = error {
+            debugPrint(error)
+        }
+        controller.dismiss(animated: true)
+    }
+
+    func addVoiceShortcutViewControllerDidCancel(_ controller: INUIAddVoiceShortcutViewController) {
+        controller.dismiss(animated: true)
+    }
+}
+
+@available(iOS 12.0, *)
+extension OrderDetailsViewController: INUIEditVoiceShortcutViewControllerDelegate {
+    func editVoiceShortcutViewController(_ controller: INUIEditVoiceShortcutViewController, didUpdate voiceShortcut: INVoiceShortcut?, error: Error?) {
+        if let error = error {
+            debugPrint(error)
+        }
+        controller.dismiss(animated: true)
+    }
+
+    func editVoiceShortcutViewController(_ controller: INUIEditVoiceShortcutViewController, didDeleteVoiceShortcutWithIdentifier deletedVoiceShortcutIdentifier: UUID) {
+        controller.dismiss(animated: true)
+    }
+
+    func editVoiceShortcutViewControllerDidCancel(_ controller: INUIEditVoiceShortcutViewController) {
+        controller.dismiss(animated: true)
     }
 }
 
