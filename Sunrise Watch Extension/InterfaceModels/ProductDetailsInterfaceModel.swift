d178ae587805c13fdaa03374b70e20ecb11100b1
Nikola Mladenovic <nikola@mladenovic.biz>
2018-12-03 18:53:31 +0100

    Remove unused watch assets

--- Sunrise Watch Extension/InterfaceModels/ProductDetailsInterfaceModel.swift
+++ Sunrise Watch Extension/InterfaceModels/ProductDetailsInterfaceModel.swift
@@ -6,14 +6,25 @@ import ReactiveSwift
 import Result
 import Commercetools
 
-class ProductDetailsInterfaceModel {
+protocol ProductDetailsInterfaceModel {
+    // Inputs
+    var moveToCartAction: Action<Void, Void, CTError>! { get }
+    
+    // Outputs
+    var productName: String { get }
+    var productImageUrl: String { get }
+    var productPrice: String? { get }
+    var productOldPrice: String { get }
+    var userActivityInfo: [AnyHashable: Any]? { get }
+    var isInWishList: Bool { get }
+}
+
+class ProductProjectionDetailsInterfaceModel: ProductDetailsInterfaceModel {
 
     // Inputs
     var moveToCartAction: Action<Void, Void, CTError>!
-    var addToWishListAction: Action<Void, Void, CTError>!
 
     // Outputs
-    let isAddToWishListEnabled = MutableProperty(true)
     var productName: String {
         return product.name.localizedString ?? ""
     }
@@ -40,6 +51,9 @@ class ProductDetailsInterfaceModel {
         guard let sku = product.displayVariant()?.sku else { return nil }
         return ["sku": sku]
     }
+    var isInWishList: Bool {
+        return mainMenuInterfaceModel?.activeWishList.value?.lineItems.contains(where: { $0.productId == product.id && $0.variantId == product.displayVariant()?.id ?? product.masterVariant.id }) == true
+    }
 
     private weak var mainMenuInterfaceModel: MainMenuInterfaceModel?
     private let product: ProductProjection
@@ -53,14 +67,6 @@ class ProductDetailsInterfaceModel {
 
         moveToCartAction = Action(enabledIf: Property(value: true)) { [unowned self] _ in
             return self.addToCart()
-        }
-        
-        addToWishListAction = Action(enabledIf: Property(value: true)) { [unowned self] _ in
-            return self.addToWishList()
-        }
-        
-        if let mainMenuInterfaceModel = mainMenuInterfaceModel {
-            disposables += isAddToWishListEnabled <~ mainMenuInterfaceModel.activeWishList.map { [weak self] in $0?.lineItems.contains(where: { $0.productId == self?.product.id && $0.variantId == self?.product.displayVariant()?.id ?? self?.product.masterVariant.id }) != true }
         }
     }
     
@@ -78,7 +84,7 @@ class ProductDetailsInterfaceModel {
             }
             DispatchQueue.global().async {
                 let activity = ProcessInfo.processInfo.beginActivity(options: [.userInitiated, .idleSystemSleepDisabled, .suddenTerminationDisabled, .automaticTerminationDisabled], reason: "Add to cart request")
-                ProductDetailsInterfaceModel.queryForActiveCart(observer: observer, activity: activity) { cart in
+                ProductProjectionDetailsInterfaceModel.queryForActiveCart(observer: observer, activity: activity) { cart in
                     let actions = [CartUpdateAction.addLineItem(lineItemDraft: LineItemDraft(productVariantSelection: .productVariant(productId: productId, variantId: variantId), quantity: 1))]
                     Cart.update(cart.id, actions: UpdateActions<CartUpdateAction>(version: cart.version, actions: actions), result: { result in
                         if result.isSuccess {
@@ -122,37 +128,5 @@ class ProductDetailsInterfaceModel {
                 })
             }
         })
-    }
-    
-    // MARK: - Add to WishList
-    
-    private func addToWishList() -> SignalProducer<Void, CTError> {
-        return SignalProducer { [weak self] observer, disposable in
-            guard let productId = self?.product.id, let variantId = self?.product.displayVariant()?.id ?? self?.product.masterVariant.id else {
-                observer.sendCompleted()
-                return
-            }
-            DispatchQueue.global().async {
-                let activity = ProcessInfo.processInfo.beginActivity(options: [.userInitiated, .idleSystemSleepDisabled, .suddenTerminationDisabled, .automaticTerminationDisabled], reason: "Add to wish list request")
-                self?.mainMenuInterfaceModel?.wishListShoppingList(observer: observer, activity: activity) { list in
-                    guard let list = list, !list.lineItems.contains(where: { $0.productId == productId && $0.variantId == variantId }) else {
-                        observer.sendCompleted()
-                        ProcessInfo.processInfo.endActivity(activity)
-                        return
-                    }
-                    let actions = [ShoppingListUpdateAction.addLineItem(productId: productId, variantId: variantId, quantity: nil, addedAt: nil, custom: nil)]
-                    ShoppingList.update(list.id, actions: UpdateActions(version: list.version, actions: actions)) { result in
-                        if let list = result.model, result.isSuccess {
-                            self?.mainMenuInterfaceModel?.activeWishList.value = list
-                            observer.send(value: ())
-                        } else if let error = result.errors?.first as? CTError, result.isFailure {
-                            observer.send(error: error)
-                        }
-                        observer.sendCompleted()
-                        ProcessInfo.processInfo.endActivity(activity)
-                    }
-                }
-            }
-        }
     }
 }
