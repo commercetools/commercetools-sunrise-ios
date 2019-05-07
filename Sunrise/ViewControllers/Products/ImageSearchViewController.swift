f3ef8cdb2ed41f48fce4ad7ecb8005e5c1f12f27
Nikola Mladenovic <nikola@mladenovic.biz>
2019-05-07 11:55:35 +0200

    Add image search view controller and view model

--- Sunrise/ViewControllers/Products/ImageSearchViewController.swift
+++ Sunrise/ViewControllers/Products/ImageSearchViewController.swift
@@ -1,9 +1,256 @@
 //
-//  ImageSearchViewController.swift
-//  Sunrise
-//
-//  Created by Nikola Mladenovic on 5/4/19.
-//  Copyright © 2019 Commercetools. All rights reserved.
+// Copyright (c) 2019 Commercetools. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import AVFoundation
+import ReactiveCocoa
+import ReactiveSwift
+import Result
+
+class ImageSearchViewController: UIViewController {
+
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var browseImageGalleryButton: UIButton!
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    let imagePickerController = UIImagePickerController()
+    private var captureSession: AVCaptureSession?
+    private let videoCaptureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+    private var previewLayer: AVCaptureVideoPreviewLayer?
+    private var liveViewCell: LiveViewCell?
+    private let photoOutput = AVCapturePhotoOutput()
+    private let disposables = CompositeDisposable()
+
+    deinit {
+        disposables.dispose()
+    }
+
+    var viewModel: ImageSearchViewModel? {
+        didSet {
+            self.bindViewModel()
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let collectionViewLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            collectionViewLayout.itemSize = ImageSearchViewModel.itemSize
+            collectionViewLayout.minimumLineSpacing = ImageSearchViewModel.itemSpacing
+            collectionViewLayout.minimumInteritemSpacing = ImageSearchViewModel.itemSpacing
+        }
+
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .photoLibrary
+
+        viewModel = ImageSearchViewModel()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        DispatchQueue.global(qos: .userInteractive).async {
+            self.startCaptureSessionAndPreview()
+        }
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        DispatchQueue.global(qos: .userInteractive).async {
+            self.stopCaptureSession()
+        }
+
+        super.viewDidDisappear(animated)
+    }
+
+    // MARK: - Bindings
+
+    private func bindViewModel() {
+        guard let viewModel = viewModel else { return }
+
+        disposables += searchButton.reactive.isHidden <~ viewModel.isSearchButtonHidden
+        disposables += browseImageGalleryButton.reactive.isHidden <~ viewModel.isBrowseAllButtonHidden
+
+        disposables += viewModel.shouldPresentPhotosAccessDeniedAlert.producer
+        .filter { $0 }
+        .observe(on: UIScheduler())
+        .startWithValues { [unowned self] _ in
+            self.presentPhotosAccessDeniedAlert()
+        }
+
+        disposables += viewModel.reloadCollectionViewSignal
+        .observe(on: UIScheduler())
+        .observeValues { [weak self] in
+            self?.collectionView.reloadData()
+            self?.startCaptureSessionAndPreview()
+        }
+
+        disposables += viewModel.captureImageSignal
+        .observe(on: UIScheduler())
+        .observeValues { [unowned self] in
+            self.photoOutput.capturePhoto(with: AVCapturePhotoSettings(format: [AVVideoCodecKey:AVVideoCodecType.jpeg]), delegate: self)
+            self.startCaptureSessionAndPreview()
+        }
+    }
+
+    private func startCaptureSessionAndPreview() {
+        if captureSession == nil {
+            captureSession = AVCaptureSession()
+            setupCaptureSessionAndPreview()
+            DispatchQueue.main.async {
+                self.collectionView.reloadItems(at: [IndexPath(item: 0, section: 0)])
+            }
+        }
+
+        if let captureSession = captureSession, !captureSession.isRunning {
+            captureSession.startRunning()
+        }
+
+        guard let captureSession = captureSession, let liveViewCell = liveViewCell else { return }
+        guard previewLayer == nil || previewLayer!.superlayer == nil else { return }
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        guard let previewLayer = previewLayer else { return }
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        DispatchQueue.main.async {
+            previewLayer.frame = liveViewCell.liveView.layer.bounds
+            liveViewCell.liveView.layer.addSublayer(previewLayer)
+        }
+    }
+
+    private func stopCaptureSession() {
+        if let captureSession = captureSession, captureSession.isRunning {
+            captureSession.stopRunning()
+        }
+    }
+
+    private func presentPhotosAccessDeniedAlert() {
+        let alertController = UIAlertController(title: NSLocalizedString("Cannot access photos", comment: "Cannot access photos"), message: NSLocalizedString("Please enable access to photos for Sunrise app", comment: "Photos permission prompt"), preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: NSLocalizedString("Settings", comment: "Settings"), style: .default) { [unowned self] _ in
+            self.navigationController?.popViewController(animated: true)
+            UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+        })
+        alertController.addAction(UIAlertAction(title: viewModel?.okAction, style: .default) { [unowned self] _ in
+            self.navigationController?.popViewController(animated: true)
+        })
+        present(alertController, animated: true)
+    }
+
+    private func setupCaptureSessionAndPreview() {
+        guard let videoCaptureDevice = videoCaptureDevice, let videoInput = try? AVCaptureDeviceInput(device: videoCaptureDevice),
+              let captureSession = captureSession, captureSession.canAddInput(videoInput) && captureSession.canAddOutput(photoOutput) else {
+            self.captureSession = nil
+            presentCaptureError()
+            return
+        }
+
+        captureSession.addInput(videoInput)
+        captureSession.addOutput(photoOutput)
+        captureSession.sessionPreset = .photo
+    }
+
+    private func presentCaptureError() {
+        let authorizationStatus = AVCaptureDevice.authorizationStatus(for: .video)
+
+        let alertController = UIAlertController(
+                title: viewModel?.liveViewErrorTitle,
+                message: authorizationStatus == .denied ? viewModel?.cameraPermissionError : viewModel?.capabilitiesError,
+                preferredStyle: .alert
+        )
+        if authorizationStatus == .denied {
+            alertController.addAction(UIAlertAction(title: viewModel?.settingsAction, style: .cancel, handler: { _ in
+                if let appSettingsURL = URL(string: UIApplication.openSettingsURLString) {
+                    UIApplication.shared.open(appSettingsURL)
+                }
+                NotificationCenter.default.post(name: Foundation.Notification.Name.Navigation.resetSearch, object: nil, userInfo: nil)
+            }))
+        }
+        alertController.addAction(UIAlertAction(title: viewModel?.okAction, style: .default))
+
+        present(alertController, animated: true)
+    }
+
+    @IBAction func browseImageGallery(_ sender: UIButton) {
+        present(imagePickerController, animated: true)
+    }
+}
+
+extension ImageSearchViewController: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel?.numberOfItems ?? 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let viewModel = viewModel else { return UICollectionViewCell() }
+
+        switch viewModel.cellType(at: indexPath) {
+        case .liveView:
+            if liveViewCell == nil {
+                liveViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "LiveViewCell", for: indexPath) as? LiveViewCell
+            }
+            liveViewCell!.selectedIconImageView.isHidden = viewModel.isSelectedIndicatorHidden(at: indexPath)
+            liveViewCell!.imageView.isHidden = viewModel.capturedImage.value == nil
+            liveViewCell!.imageView.image = viewModel.capturedImage.value
+            return liveViewCell!
+        case .image:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as? PhotoCell else { return UICollectionViewCell() }
+            cell.imageView.image = nil
+            cell.selectedIconImageView.isHidden = viewModel.isSelectedIndicatorHidden(at: indexPath)
+            viewModel.image(at: indexPath, completion: { image in
+                let currentIndexPath = collectionView.indexPath(for: cell)
+                guard currentIndexPath == nil || currentIndexPath == indexPath else { return }
+                cell.imageView.image = image
+            })
+            return cell
+        }
+    }
+}
+
+extension ImageSearchViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let previouslySelectedIndexPath = viewModel?.selectedItemIndexPath.value
+        viewModel?.selectedItemIndexPath.value = indexPath
+        guard indexPath != previouslySelectedIndexPath else { return }
+        var indexPathsToReload = [IndexPath]()
+        if let previouslySelectedIndexPath = previouslySelectedIndexPath {
+            indexPathsToReload.append(previouslySelectedIndexPath)
+        }
+        if indexPath.row != 0 {
+            indexPathsToReload.append(indexPath)
+        }
+        collectionView.reloadItems(at: indexPathsToReload)
+    }
+}
+
+extension ImageSearchViewController: UICollectionViewDataSourcePrefetching {
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        viewModel?.prefetchItemsObserver.send(value: indexPaths)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cancelPrefetchingForItemsAt indexPaths: [IndexPath]) {
+        viewModel?.cancelPrefetchingObserver.send(value: indexPaths)
+    }
+}
+
+extension ImageSearchViewController: UIImagePickerControllerDelegate {
+
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }
+
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        picker.dismiss(animated: true)
+    }
+}
+
+extension ImageSearchViewController: UINavigationControllerDelegate {
+
+}
+
+extension ImageSearchViewController: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        guard let data = photo.fileDataRepresentation() else { return }
+        viewModel?.capturedImage.value = UIImage(data: data)
+        collectionView.reloadItems(at: [IndexPath(item: 0, section: 0)])
+    }
+}
--- Sunrise/ViewModels/Products/ImageSearchViewModel.swift
+++ Sunrise/ViewModels/Products/ImageSearchViewModel.swift
@@ -1,9 +1,177 @@
 //
-//  ImageSearchViewModel.swift
-//  Sunrise
-//
-//  Created by Nikola Mladenovic on 5/4/19.
-//  Copyright © 2019 Commercetools. All rights reserved.
+// Copyright (c) 2019 Commercetools. All rights reserved.
 //
 
-import Foundation
+import ReactiveSwift
+import Result
+import Photos
+
+class ImageSearchViewModel: NSObject {
+
+    enum CellType {
+        case liveView
+        case image
+    }
+
+    // Inputs
+    let dismissObserver: Signal<Void, NoError>.Observer
+    let resetImageSelectionObserver: Signal<Void, NoError>.Observer
+    let prefetchItemsObserver: Signal<[IndexPath], NoError>.Observer
+    let cancelPrefetchingObserver: Signal<[IndexPath], NoError>.Observer
+    let selectedItemIndexPath = MutableProperty<IndexPath?>(nil)
+    let capturedImage = MutableProperty<UIImage?>(nil)
+
+    // Outputs
+    let reloadCollectionViewSignal: Signal<Void, NoError>
+    let dismissSignal: Signal<Void, NoError>
+    let captureImageSignal: Signal<Void, NoError>
+    let shouldPresentPhotosAccessDeniedAlert = MutableProperty(false)
+    let isSearchButtonHidden = MutableProperty(true)
+    let isBrowseAllButtonHidden = MutableProperty(true)
+
+    private let reloadCollectionViewObserver: Signal<Void, NoError>.Observer
+    private var imageManager: PHCachingImageManager?
+    private var fetchResult: PHFetchResult<PHAsset>?
+    private let disposables = CompositeDisposable()
+
+    private static let photosPerRow: Int = 3
+    private static let leadingTrailingSpace: CGFloat = 20.0
+    static let itemSpacing: CGFloat = 6.0
+    static let itemSize: CGSize = {
+        let availableWidthForItems = UIScreen.main.bounds.width - CGFloat(photosPerRow - 1) * itemSpacing - leadingTrailingSpace
+        let itemWidth = availableWidthForItems / CGFloat(photosPerRow)
+        return CGSize(width: itemWidth, height: itemWidth)
+    }()
+
+    // Dialogue texts
+    let okAction = NSLocalizedString("OK", comment: "OK")
+    let settingsAction = NSLocalizedString("Settings", comment: "Settings")
+    let liveViewErrorTitle = NSLocalizedString("Could not initialize live view", comment: "Image search live view error title")
+    let cameraPermissionError = NSLocalizedString("In order to capture an image using live view, please go to settings and grant Camera permission.", comment: "Camera permissions error")
+    let capabilitiesError = NSLocalizedString("Your device is not capable of capturing an image using live view.", comment: "Capabilities live view error")
+
+    // MARK: Lifecycle
+
+    override init() {
+        (reloadCollectionViewSignal, reloadCollectionViewObserver) = Signal<Void, NoError>.pipe()
+        (dismissSignal, dismissObserver) = Signal<Void, NoError>.pipe()
+
+        let (resetImageSelectionSignal, resetImageSelectionObserver) = Signal<Void, NoError>.pipe()
+        self.resetImageSelectionObserver = resetImageSelectionObserver
+
+        let (prefetchItemsSignal, prefetchItemsObserver) = Signal<[IndexPath], NoError>.pipe()
+        self.prefetchItemsObserver = prefetchItemsObserver
+
+        let (cancelPrefetchingSignal, cancelPrefetchingObserver) = Signal<[IndexPath], NoError>.pipe()
+        self.cancelPrefetchingObserver = cancelPrefetchingObserver
+
+        let (captureImageSignal, captureImageObserver) = Signal<Void, NoError>.pipe()
+        self.captureImageSignal = captureImageSignal
+
+        super.init()
+
+        disposables += prefetchItemsSignal.observeValues { [weak self] indexPaths in
+            guard let fetchResult = self?.fetchResult else { return }
+            let assets = indexPaths.map { fetchResult.object(at: $0.item) }
+            self?.imageManager?.startCachingImages(for: assets, targetSize: ProfilePhotoViewModel.itemSize, contentMode: .aspectFill, options: nil)
+        }
+
+        disposables += cancelPrefetchingSignal.observeValues { [weak self] indexPaths in
+            guard let fetchResult = self?.fetchResult else { return }
+            let assets = indexPaths.map { fetchResult.object(at: $0.item) }
+            self?.imageManager?.stopCachingImages(for: assets, targetSize: ProfilePhotoViewModel.itemSize, contentMode: .aspectFill, options: nil)
+        }
+
+        disposables += resetImageSelectionSignal.observeValues { [weak self] in
+            self?.reloadCollectionViewObserver.send(value: ())
+        }
+
+        disposables += selectedItemIndexPath.signal
+        .filter { $0?.item == 0 }
+        .observeValues { _ in
+            captureImageObserver.send(value: ())
+        }
+
+        disposables += selectedItemIndexPath.signal
+        .filter { $0?.item != 0 }
+        .observeValues { [weak self] _ in
+            self?.capturedImage.value = nil
+        }
+
+        disposables += selectedItemIndexPath <~ resetImageSelectionSignal.map { nil }
+        disposables += capturedImage <~ resetImageSelectionSignal.map { nil }
+        disposables += isSearchButtonHidden <~ selectedItemIndexPath.map { $0 == nil }
+        disposables += isBrowseAllButtonHidden <~ selectedItemIndexPath.map { $0 != nil }
+
+        PHPhotoLibrary.shared().register(self)
+
+        switch PHPhotoLibrary.authorizationStatus() {
+            case .authorized:
+                reloadPhotos()
+            case .notDetermined:
+                PHPhotoLibrary.requestAuthorization { [weak self] status in
+                    guard status == .authorized else {
+                        self?.shouldPresentPhotosAccessDeniedAlert.value = true
+                        return
+                    }
+                    self?.reloadPhotos()
+                }
+            default:
+                shouldPresentPhotosAccessDeniedAlert.value = true
+        }
+    }
+
+    deinit {
+        PHPhotoLibrary.shared().unregisterChangeObserver(self)
+        disposables.dispose()
+    }
+
+    func requestAuthorizations() {
+    }
+
+    // MARK: - Data Source
+
+    var numberOfItems: Int {
+        return (fetchResult?.count ?? 0) + 1
+    }
+
+    func cellType(at indexPath: IndexPath) -> CellType {
+        return indexPath.item == 0 ? .liveView : .image
+    }
+
+    func image(at indexPath: IndexPath, completion: @escaping (UIImage?) -> Void) {
+        guard let fetchResult = fetchResult else {
+            completion(nil)
+            return
+        }
+        let phAsset = fetchResult.object(at: indexPath.row - 1)
+        imageManager?.requestImage(for: phAsset, targetSize: ProfilePhotoViewModel.itemSize, contentMode: .aspectFill, options: nil) { (image, _) in
+            completion(image)
+        }
+    }
+
+    func isSelectedIndicatorHidden(at indexPath: IndexPath) -> Bool {
+        return indexPath != selectedItemIndexPath.value
+    }
+
+    // MARK: - Loading photos from the library
+
+    private func reloadPhotos() {
+        DispatchQueue.global().async { [weak self] in
+            let fetchOptions = PHFetchOptions()
+            fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+            let results = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+            DispatchQueue.main.async {
+                self?.imageManager = self?.imageManager ?? PHCachingImageManager()
+                self?.fetchResult = results
+                self?.reloadCollectionViewObserver.send(value: ())
+            }
+        }
+    }
+}
+
+extension ImageSearchViewModel: PHPhotoLibraryChangeObserver {
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        reloadPhotos()
+    }
+}
