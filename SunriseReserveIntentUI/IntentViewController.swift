c67133c624fb2d92e33d929ab8ec34555c4fccb2
Nikola Mladenovic <nikola@mladenovic.biz>
2019-08-05 14:03:26 +0200

    IntentViewController and IntentViewModel for reservation intent extension

--- SunriseReserveIntentUI/IntentViewController.swift
+++ SunriseReserveIntentUI/IntentViewController.swift
@@ -1,29 +1,134 @@
 import IntentsUI
-
-// As an example, this extension's Info.plist has been configured to handle interactions for INSendMessageIntent.
-// You will want to replace this or add other intents as appropriate.
-// The intents whose interactions you wish to handle must be declared in the extension's Info.plist.
-
-// You can test this example integration by saying things to Siri like:
-// "Send a message using <myApp>"
+import MapKit
+import ReactiveSwift
+import ReactiveCocoa
+import SDWebImage
 
 class IntentViewController: UIViewController, INUIHostedViewControlling {
-    
+
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var productColorView: UIView!
+
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var storeNameLabel: UILabel!
+    @IBOutlet weak var openHoursLabel: UILabel!
+    @IBOutlet weak var storeAddressLabel: UILabel!
+    @IBOutlet weak var sizeLabel: UILabel!
+    @IBOutlet weak var quantityLabel: UILabel!
+
+    private let locationManager = CLLocationManager()
+    private var configureViewCompletion: ((Bool, Set<INParameter>, CGSize) -> Void)?
+    private let disposables = CompositeDisposable()
+
+    deinit {
+        disposables.dispose()
+    }
+
+    var viewModel: IntentViewModel? {
+        didSet {
+            bindViewModel()
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+
+        locationManager.delegate = self
+        locationManager.distanceFilter = 50
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
         
+        viewModel = IntentViewModel()
+    }
+    
+    private func bindViewModel() {
+        guard let viewModel = viewModel else { return }
+        
+        disposables += productNameLabel.reactive.text <~ viewModel.productName
+        disposables += distanceLabel.reactive.text <~ viewModel.distance
+        disposables += storeNameLabel.reactive.text <~ viewModel.storeName
+        disposables += openHoursLabel.reactive.text <~ viewModel.openingTimes
+        disposables += storeAddressLabel.reactive.text <~ viewModel.storeAddress
+        disposables += sizeLabel.reactive.text <~ viewModel.size
+        disposables += quantityLabel.reactive.text <~ viewModel.quantity
+        
+        disposables += viewModel.visibleMapRect.producer
+        .observe(on: UIScheduler())
+        .startWithValues { [weak self] visibleRegion in
+            self?.mapView.setVisibleMapRect(visibleRegion, edgePadding: UIEdgeInsets(top: 60, left: 60, bottom: 60, right: 60), animated: true)
+        }
+        
+        disposables += viewModel.channel.producer
+        .filter { $0 != nil }
+        .observe(on: UIScheduler())
+        .startWithValues { [weak self] storeLocation in
+            guard let mapView = self?.mapView, let coordinate = storeLocation?.location?.coordinate else { return }
+            mapView.removeAnnotations(mapView.annotations)
+            let storeAnnotation =  MKPointAnnotation()
+            storeAnnotation.coordinate = coordinate
+            mapView.addAnnotation(storeAnnotation)
+        }
+        
+        disposables += viewModel.productColor.producer
+        .observe(on: UIScheduler())
+        .startWithValues { [unowned self] in
+            self.productColorView.backgroundColor = $0
+        }
+        
+        disposables += viewModel.productImageUrl.producer
+        .observe(on: UIScheduler())
+        .startWithValues { [unowned self] in
+            self.productImageView.sd_setImage(with: URL(string: $0))
+        }
+        
+        disposables += viewModel.isLoading.producer
+        .filter { !$0 }
+        .observe(on: UIScheduler())
+        .startWithValues { [unowned self] _ in
+            self.configureViewCompletion?(true, Set(), self.desiredSize)
+        }
+        
+        disposables += viewModel.errorSignal
+        .observe(on: UIScheduler())
+        .observeValues { [weak self] in
+            self?.configureViewCompletion?(false, Set(), .zero)
+        }
+    }
+    
     // MARK: - INUIHostedViewControlling
     
-    // Prepare your view controller for the interaction to handle.
     func configureView(for parameters: Set<INParameter>, of interaction: INInteraction, interactiveBehavior: INUIInteractiveBehavior, context: INUIHostedViewContext, completion: @escaping (Bool, Set<INParameter>, CGSize) -> Void) {
-        // Do configuration here, including preparing views and calculating a desired size for presentation.
-        completion(true, parameters, self.desiredSize)
+        guard let intent = interaction.intent as? ReserveProductIntent, let previousReservationId = intent.previousReservationId else {
+            completion(false, Set(), .zero)
+            return
+        }
+        viewModel?.previousReservationIdObserver.send(value: previousReservationId)
+        configureViewCompletion = completion
     }
     
     var desiredSize: CGSize {
-        return self.extensionContext!.hostedViewMaximumAllowedSize
+        return CGSize(width: extensionContext!.hostedViewMaximumAllowedSize.width, height: 501)
+    }
+}
+
+extension IntentViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        viewModel?.userLocation.value = locations.last
     }
     
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        debugPrint(error)
+    }
+}
+
+extension IntentViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard !(annotation is MKUserLocation) else { return nil }
+        let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "activeStoreAnnotation")
+        annotationView.image =  #imageLiteral(resourceName: "map_pin_active")
+        return annotationView
+    }
 }
--- SunriseReserveIntentUI/IntentViewModel.swift
+++ SunriseReserveIntentUI/IntentViewModel.swift
@@ -1,1 +1,111 @@
+//
+// Copyright (c) 2019 Commercetools. All rights reserved.
+//
 
+import Foundation
+import Commercetools
+import ReactiveSwift
+import Result
+import MapKit
+
+class IntentViewModel: BaseViewModel {
+    
+    // Inputs
+    let userLocation: MutableProperty<CLLocation?> = MutableProperty(nil)
+    
+    // Outputs
+    let channel = MutableProperty<Channel?>(nil)
+    let isLoading = MutableProperty(true)
+    let visibleMapRect = MutableProperty(MKMapRect.null)
+    let productName: MutableProperty<String?> = MutableProperty(nil)
+    let productImageUrl = MutableProperty("")
+    let distance: MutableProperty<String?> = MutableProperty(nil)
+    let storeName: MutableProperty<String?> = MutableProperty(nil)
+    let openingTimes: MutableProperty<String?> = MutableProperty(nil)
+    let storeAddress: MutableProperty<String?> = MutableProperty(nil)
+    let size: MutableProperty<String?> = MutableProperty(nil)
+    let quantity = MutableProperty<String?>("x1")
+    let productColor: MutableProperty<UIColor?> = MutableProperty(nil)
+    
+    // Inputs
+    let previousReservationIdObserver: Signal<String, NoError>.Observer
+    
+    // Outputs
+    let numberOfRows = MutableProperty(0)
+    let orderTotal = MutableProperty("")
+    let errorSignal: Signal<Void, NoError>
+
+    private let lineItem = MutableProperty<LineItem?>(nil)
+    private let errorObserver: Signal<Void, NoError>.Observer
+    private let distanceFormatter = MKDistanceFormatter()
+    private let disposables = CompositeDisposable()
+    
+    override init() {
+        (errorSignal, errorObserver) = Signal<Void, NoError>.pipe()
+        let (previousReservationIdSignal, previousReservationIdObserver) = Signal<String, NoError>.pipe()
+        self.previousReservationIdObserver = previousReservationIdObserver
+        
+        if let configuration = Project.config {
+            Commercetools.config = configuration
+        }
+        
+        super.init()
+        
+        distanceFormatter.unitStyle = .abbreviated
+        
+        disposables += previousReservationIdSignal
+        .observeValues { [weak self] in
+            self?.retrieveReservation(by: $0)
+        }
+        
+        disposables += productName <~ lineItem.map { $0?.name.localizedString }
+        disposables += productImageUrl <~ lineItem.map { $0?.variant.images?.first?.url ?? "" }
+        disposables += storeName <~ channel.map { $0?.name?.localizedString }
+        disposables += openingTimes <~ channel.map { $0?.openingTimes }
+        disposables += storeAddress <~ channel.map { "\($0?.streetAndNumberInfo ?? "")\n\($0?.zipAndCityInfo ?? "")" }
+        disposables += size <~ lineItem.map { $0?.variant.attributes?.first(where: { $0.name == Attribute.kSizeAttributeName })?.valueLabel }
+        disposables += productColor <~ lineItem.map { (lineItem: LineItem?) -> UIColor? in
+            guard let colorValue = lineItem?.variant.attributes?.first(where: { $0.name == Attribute.kColorsAttributeName })?.valueKey else { return nil }
+            return Attribute.colorValues[colorValue]
+        }
+        disposables += distance <~ channel.combineLatest(with: userLocation).map { [weak self] store, userLocation -> String? in
+            guard let store = store, let userLocation = userLocation, let distance = store.distance(from: userLocation) else { return "-" }
+            return self?.distanceFormatter.string(fromDistance: distance)
+        }
+        
+        disposables += visibleMapRect <~ userLocation.combineLatest(with: channel).map { userLocation, channel in
+            var visibleLocations = [CLLocation]()
+            if let userLocation = userLocation {
+                visibleLocations.append(userLocation)
+            }
+            if let channelLocation = channel?.location {
+                visibleLocations.append(channelLocation)
+            }
+            
+            var zoomRect = MKMapRect.null
+            let visibleRects = visibleLocations.map { location in
+                MKMapRect(origin: MKMapPoint(location.coordinate), size: MKMapSize(width: 0.1, height: 0.1))
+            }
+            visibleRects.forEach {
+                zoomRect = zoomRect.union($0)
+            }
+            return zoomRect
+        }
+    }
+    
+    deinit {
+        disposables.dispose()
+    }
+    
+    private func retrieveReservation(by id: String) {
+        Order.byId(id, expansion: ["lineItems[0].distributionChannel"]) { result in
+            self.isLoading.value = false
+            guard let previousReservation = result.model, let lineItem = previousReservation.lineItems.first, let channel = lineItem.distributionChannel?.obj, result.isSuccess else {
+                self.errorObserver.send(value: ())
+                return
+            }
+            self.lineItem.value = lineItem
+            self.channel.value = channel
+        }
+    }
+}
